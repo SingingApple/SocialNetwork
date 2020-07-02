f8847ff6fecbc320402ed9c441bb385c958c4ea5
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-07-02 23:45:03 +0530

    React-Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar></Navbar>
+      <Landing></Landing>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
