a555d2875ebbd3dd0254d1128884fe7e7e956ca3
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-07-03 00:05:55 +0530

    Auth React Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Navbar></Navbar>
-      <Landing></Landing>
-    </React.Fragment>
+    <Router>
+      <React.Fragment>
+        <Navbar></Navbar>
+        <Route path="/" exact component={Landing}></Route>
+        <section className="container">
+          <Switch>
+            <Route path="/register" exact component={Register}></Route>
+            <Route path="/login" exact component={Login}></Route>
+          </Switch>
+        </section>
+      </React.Fragment>
+    </Router>
   );
 };
 
