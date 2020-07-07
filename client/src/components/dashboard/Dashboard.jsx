3a36da5f1a9e359a0c9e9b1d1c2f40d1a3aacaa2
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-07-08 01:10:45 +0530

    Added profile

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, Fragment } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
 import Spinner from "../layout/Spinner";
@@ -13,10 +14,20 @@ const Dashboard = () => {
     <Spinner />
   ) : (
     <Fragment>
-      <h1 className="large text-primary"></h1>
+      <h1 className="large text-primary">Dashboard</h1>
       <p className="lead">
         <i className="fas fa-user"></i> Welcome {auth.user && auth.user.name}
       </p>
+      {profile.profile !== null ? (
+        <Fragment>has</Fragment>
+      ) : (
+        <Fragment>
+          Click on the link below to create a profile <br />
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
