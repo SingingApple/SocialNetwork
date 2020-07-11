77341117b76122cd568fe5c47e4b182fa9de19f0
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-07-11 17:22:07 +0530

    All Post comp

---
+++ client/src/components/posts/PostItem.jsx
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import Moment from "react-moment";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+const PostItem = ({
+  post: { _id, text, name, avatar, user, likes, comments, date },
+}) => {
+  return (
+    <div className="post bg-white p-1 my-1">
+      <div>
+        <a href="profile.html">
+          <img className="round-img" src={avatar} />
+          <h4>{name}</h4>
+        </a>
+      </div>
+      <div>
+        <p className="my-1">{text}</p>
+        <p className="post-date">
+          Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
+        </p>
+        <button type="button" className="btn btn-light">
+          <i className="fas fa-thumbs-up"></i>
+          <span>{likes.length > 0 && likes.length}</span>
+        </button>
+        <button type="button" className="btn btn-light">
+          <i className="fas fa-thumbs-down"></i>
+        </button>
+        <Link to={`/post/${_id}`} className="btn btn-primary">
+          Discussion{" "}
+          <span className="comment-count">
+            {comments.length > 0 && comments.length}
+          </span>
+        </Link>
+        <button type="button" className="btn btn-danger">
+          <i className="fas fa-times"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+export default PostItem;
--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -3,9 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { getPosts } from "../../actions/post";
 import Spinner from "../layout/Spinner";
 
+import PostItem from "./PostItem";
+
 const Posts = () => {
   const storePost = useSelector((state) => state.post);
-  const { post, loading } = storePost;
+  const { posts, loading } = storePost;
   const dispatch = useDispatch();
   useEffect(() => {
     function getData() {
@@ -13,6 +15,19 @@ const Posts = () => {
     }
     getData();
   }, [dispatch]);
-  return <Fragment></Fragment>;
+  return loading ? (
+    <Spinner></Spinner>
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>Welcome to the community
+      </p>
+      {/* POST FORM*/}
+      {posts.map((post) => (
+        <PostItem key={post._id} post={post}></PostItem>
+      ))}
+    </Fragment>
+  );
 };
 export default Posts;
